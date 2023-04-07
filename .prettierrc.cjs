220daf279adccffe6363b0974b92d648546527f5
zhongyi.li <1454598684@qq.com>
2023-04-07 16:48:46 +0800

    chore: 添加.npmrc文件&&prettier配置文件

---
+++ .prettierrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+  // 使用 prettier 的配置 schema。
+  $schema: "https://json.schemastore.org/prettierrc",
+
+  // // 将箭头函数的参数始终用括号包裹起来。
+  // arrowParens: 'always',
+
+  // // 在同一行中书写对象大括号和数组大括号。
+  // bracketSameLine: true,
+
+  // // 在对象大括号内不添加空格。
+  // bracketSpacing: false,
+
+  // // 根据需要自动格式化嵌入式语言，如在模板字符串中的 JavaScript 代码。
+  // embeddedLanguageFormatting: 'auto',
+
+  // // HTML 空格敏感性使用 css。
+  // htmlWhitespaceSensitivity: 'css',
+
+  // // 不使用单引号而是使用双引号来包裹 JSX 属性。
+  // jsxSingleQuote: false,
+
+  // // 设置每行的字符数限制，超过则格式化。
+  // printWidth: 120,
+
+  // // 不在 markdown 文件中强制断行。
+  // proseWrap: 'preserve',
+
+  // // 只有在必要时才将属性名称用引号包裹起来，例如包含空格或保留关键字的属性名。
+  // quoteProps: 'as-needed',
+
+  // // 不插入任何格式化代码，例如 Prettier 的特殊注释。
+  // insertPragma: false,
+
+  // // 不要在格式化的文件中要求使用特殊注释。
+  // requirePragma: false,
+
+  // // 在语句末尾添加分号。
+  // semi: true,
+
+  // // 使用单引号而不是双引号。
+  // singleQuote: true,
+
+  // // 设置一个 tab 的宽度。
+  // tabWidth: 2,
+
+  // // 不在多行的末尾添加逗号。
+  // trailingComma: 'none',
+
+  // // 使用空格而不是制表符来缩进。
+  // useTabs: false,
+
+  // // 不缩进 .vue 文件中的 <script> 和 <style> 标签。
+  // vueIndentScriptAndStyle: false
+};
--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 # electron-template
 
-## Project setup
+## 安装项目依赖
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
