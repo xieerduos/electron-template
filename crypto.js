fde0e64e93b3081ba1a9a4d6a4c831cf2d13d1f7
zhongyi.li <1454598684@qq.com>
2023-04-07 18:59:41 +0800

    fix: 解决node 18.x以上版本报错问题

---
+++ crypto.js
@@ -0,0 +1,32 @@
+// https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/1885#issuecomment-1316735840
+
+// 解决node版本过高打包错误问题
+
+/***
+ * -  Bundling main process...node:internal/crypto/hash:71
+  this[kHandle] = new _Hash(algorithm, xofLen);        
+                  ^
+Error: error:0308010C:digital envelope routines::unsupported
+    at new Hash (node:internal/crypto/hash:71:19)
+    at Object.createHash (node:crypto:133:10)
+    at module.exports (D:\Desktop\2-electron-video\node_modules\vue-cli-plugin-electron-builder\node_modules\webpack\lib\util\createHash.js:135:53)
+    at NormalModule._initBuildHash (D:\Desktop\2-electron-video\node_modules\vue-cli-plugin-electron-builder\node_modules\webpack\lib\NormalModule.js:417:16)
+ */
+
+const crypto = require('crypto');
+
+module.exports = function useCrypto() {
+  /**
+   * md4 algorithm is not available anymore in NodeJS 17+ (because of lib SSL 3).
+   * In that case, silently replace md4 by md5 algorithm.
+   */
+  try {
+    crypto.createHash('md4');
+  } catch (e) {
+    console.warn('Crypto "md4" is not supported anymore by this Node version');
+    const origCreateHash = crypto.createHash;
+    crypto.createHash = (alg, opts) => {
+      return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+    };
+  }
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const useCrypto = require('./crypto.js');
+
+useCrypto();
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
