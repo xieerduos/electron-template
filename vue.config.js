a16609bd13bacc58cd12df51d5b6caca535c82a3
zhongyi.li <1454598684@qq.com>
2023-04-08 23:30:56 +0800

    refactor: 调整代码目录

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,38 @@ const useCrypto = require('./crypto.js');
 useCrypto();
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  pages: {
+    index: {
+      entry: 'src/renderer/index/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        asarUnpack: ['sdk'],
+        // extraResources: [
+        //   {
+        //     from: "sdk/mymain.dll",
+        //     to: "sdk/mymain.dll",
+        //     filter: ["**/*"],
+        //   },
+        // ],
+        mac: {
+          hardenedRuntime: true,
+          extendInfo: {
+            NSMicrophoneUsageDescription: true, // 请允许本程序访问您的麦克风
+            NSCameraUsageDescription: true // 请允许本程序访问您的摄像头
+          }
+        }
+        // options placed here will be merged with default configuration and passed to electron-builder
+      },
+      nodeIntegration: false,
+      externals: ['ffi-napi', 'ref-napi'],
+      mainProcessFile: 'src/main/background.js',
+      mainProcessWatch: ['src/main/**/*']
+    }
+  }
 });
