d43f7e353184a52880eb28f189f9de7841620a70
程序员李钟意 <1454598684@qq.com>
2023-11-05 01:50:20 +0800

    feat(background.js): 增加单例模式
    - 增加单例模式,第二个实例打开的时候显示主窗口
    - 全局捕获未捕获的异常错误
    - 自定义协议app支持supportFetchAPI true 解决web Assembly加载失败问题

--- src/main/background.js
+++ src/main/background.js
@@ -6,71 +6,96 @@ import path from 'path';
 import {handleSetTitle} from '@/main/utils/setTitle.js';
 import {handleFileOpen} from '@/main/utils/openDialog.js';
 
+const log = require('@/main/log/index.js');
 const {createWindow, setQuitting, getMainWindow} = require('@/main/windows/mainWindow.js');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-// Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: {secure: true, standard: true}}]);
-
-app.on('before-quit', () => {
-  setQuitting(true);
+// 全局捕获未捕获的异常错误
+process.on('uncaughtException', (error) => {
+  log.error('[background.js 未捕获的异常错误]', error);
 });
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+const gotTheLock = app.requestSingleInstanceLock();
 
-app.on('activate', () => {
-  const mainWindow = getMainWindow({createIfNotFound: true});
-  if (mainWindow.isMinimized()) {
-    mainWindow.restore();
-  }
-  mainWindow.show();
-});
+if (!gotTheLock) {
+  // 已经存在实例退出
+  log.info('quit gotTheLock', gotTheLock);
+  app.quit();
+} else {
+  protocol.registerSchemesAsPrivileged([
+    // https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/607#issuecomment-569469770
+    {scheme: 'app', privileges: {secure: true, supportFetchAPI: true, standard: true}}
+  ]);
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // 当运行第二个实例时,将会聚焦到mainWindow这个窗口
+    log.info('[second-instance commandLine]', commandLine);
+    log.info('[second-instance workingDirectory]', workingDirectory);
+    const mainWindow = getMainWindow({createIfNotFound: true});
+    log.info('[second-instance mainWindow]', mainWindow && !mainWindow.isDestroyed());
+    if (mainWindow && !mainWindow.isDestroyed()) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.show();
+    }
+  });
+  app.on('before-quit', () => {
+    setQuitting(true);
+  });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  createProtocol('app');
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-  if (isDevelopment && !process.env.IS_TEST) {
-    const extensionFolder = path.join(__static, '../vue_devtools/');
-    session.defaultSession
-      .loadExtension(extensionFolder)
-      .then((res) => {
-        // console.log('[vue_devtools res]', res);
-      })
-      .catch((err) => {
-        console.error('[vue_devtools err]', err);
-      });
-  }
-  ipcMain.on('set-title', handleSetTitle);
-  ipcMain.handle('dialog:openFile', handleFileOpen);
-  createWindow();
-  // const sdk = require('@/main/sdk/index.js');
-  const tray = require('@/main/tray/index.js');
-  const settings = require('@/main/settings/index.js');
+  app.on('activate', () => {
+    const mainWindow = getMainWindow({createIfNotFound: true});
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore();
+    }
+    mainWindow.show();
+  });
 
-  // if (process.platform === 'win32') {
-  //   sdk.initialize();
-  // }
-  tray.initialize();
-  settings.initialize();
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', async () => {
+    createProtocol('app');
 
-  if (process.platform === 'darwin') {
-    // 仅在 macOS 上设置 Dock 图标
-    const iconPath = path.join(__static, 'icons/png/512x512.png');
-    app.dock.setIcon(iconPath);
-  }
-});
+    if (isDevelopment && !process.env.IS_TEST) {
+      const extensionFolder = path.join(__static, '../vue_devtools/');
+      session.defaultSession
+        .loadExtension(extensionFolder)
+        .then((res) => {
+          // console.log('[vue_devtools res]', res);
+        })
+        .catch((err) => {
+          console.error('[vue_devtools err]', err);
+        });
+    }
+    ipcMain.on('set-title', handleSetTitle);
+    ipcMain.handle('dialog:openFile', handleFileOpen);
+    createWindow();
+    // const sdk = require('@/main/sdk/index.js');
+    const tray = require('@/main/tray/index.js');
+    const settings = require('@/main/settings/index.js');
 
+    // if (process.platform === 'win32') {
+    //   sdk.initialize();
+    // }
+    tray.initialize();
+    settings.initialize();
+
+    if (process.platform === 'darwin') {
+      // 仅在 macOS 上设置 Dock 图标
+      const iconPath = path.join(__static, 'icons/png/512x512.png');
+      app.dock.setIcon(iconPath);
+    }
+  });
+}
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
